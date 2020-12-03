97049cc6d8491aa62e475616622a125240441448
Ben Tomasini <btomasini@activated.io>
2020-12-02 21:25:16 -0800

    Fixed outputs

--- modules/vpc-network/outputs.tf
+++ modules/vpc-network/outputs.tf
@@ -39,28 +39,28 @@ output "public_subnetwork_secondary_range_name" {
 
 output "private_subnetwork" {
   description = "A reference (self_link) to the private subnetwork"
-  value       = google_compute_subnetwork.vpc_subnetwork_private.self_link
+  value       = google_compute_subnetwork.vpc_subnetwork_private.*.self_link
 }
 
 output "private_subnetwork_name" {
   description = "Name of the private subnetwork"
-  value       = google_compute_subnetwork.vpc_subnetwork_private.name
+  value       = google_compute_subnetwork.vpc_subnetwork_private.*.name
 }
 
 output "private_subnetwork_cidr_block" {
-  value = google_compute_subnetwork.vpc_subnetwork_private.ip_cidr_range
+  value = google_compute_subnetwork.vpc_subnetwork_private.*.ip_cidr_range
 }
 
 output "private_subnetwork_gateway" {
-  value = google_compute_subnetwork.vpc_subnetwork_private.gateway_address
+  value = google_compute_subnetwork.vpc_subnetwork_private.*.gateway_address
 }
 
 output "private_subnetwork_secondary_cidr_block" {
-  value = google_compute_subnetwork.vpc_subnetwork_private.secondary_ip_range[0].ip_cidr_range
+  value = google_compute_subnetwork.vpc_subnetwork_private.*.secondary_ip_range[0].ip_cidr_range
 }
 
 output "private_subnetwork_secondary_range_name" {
-  value = google_compute_subnetwork.vpc_subnetwork_private.secondary_ip_range[0].range_name
+  value = google_compute_subnetwork.vpc_subnetwork_private.*.secondary_ip_range[0].range_name
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
